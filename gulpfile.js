595ab57ca85c9cceaebf63d88e0594df3d70b9f9
Miguel Obando <miguel@obando.com.ve>
2015-11-19 19:54:25 -0500

    added livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,36 @@
-'use strict';
-
-var watchify = require('watchify');
-var browserify = require('browserify');
+var http = require('http');
 var gulp = require('gulp');
-var source = require('vinyl-source-stream');
-var buffer = require('vinyl-buffer');
-var gutil = require('gulp-util');
-var sourcemaps = require('gulp-sourcemaps');
-var assign = require('lodash').assign;
+var browserify = require('gulp-browserify');
+var concat = require('gulp-concat');
+var less = require('gulp-less');
+var refresh = require('gulp-livereload');
+var lr = require('tiny-lr');
+var ecstatic = require('ecstatic');
+var lrserver = lr();
 
-// add custom browserify options here
-var customOpts = {
-  entries: ['./src/app.js'],
-  debug: true
-};
-var opts = assign({}, watchify.args, customOpts);
-var b = watchify(browserify(opts)); 
+var livereloadport = 35729,
+    serverport = 5001;
+
+gulp.task('scripts', function() {
+    return gulp.src(['src/**/*.js'])
+        .pipe(browserify())
+        .pipe(concat('app.js'))
+        .pipe(gulp.dest('./build'))
+        .pipe(refresh(lrserver));
+});
+
+gulp.task('serve', function() {
+  //Set up your static fileserver, which serves files in the build dir
+  http.createServer(ecstatic({ root: __dirname + '/build' })).listen(serverport);
+
+  //Set up your livereload server
+  lrserver.listen(livereloadport);
+});
 
 
-gulp.task('default', bundle); 
-b.on('update', bundle); 
-b.on('log', gutil.log); 
+// Requires gulp >=v3.5.0
+gulp.task('watch', function () {
+    gulp.watch('src/**', ['scripts']);
+});
 
-function bundle() {
-  return b.bundle()
-    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
-    .pipe(source('app.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({loadMaps: true})) 
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest('./build'));
-}
+gulp.task('default', ['scripts', 'serve', 'watch']);
